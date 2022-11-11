1089b44bbac8329d599e4d42b428f562281d71fe
johnchri <johnchri@student.chalmers.se>
2022-11-11 13:17:37 +0100

    Update BookingRequests

--- server/models/bookingRequest.js
+++ server/models/bookingRequest.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose")
 var Schema = mongoose.Schema
 
 var bookingRequestSchema = new Schema({
-request_id : {type : String, unique: true},
 user_id : {type: String},
 dentist_id : {type: Schema.Types.ObjectId, ref: 'Dentist'},
 issuance : {type: String, unique: true},
