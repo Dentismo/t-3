f275ea3ab924475256b16eb1e8a03960985be5ad
johnchri <johnchri@student.chalmers.se>
2022-11-16 20:48:30 +0100

    #2 Update bookings controller

--- server/controllers/bookingRequests.js
+++ server/controllers/bookingRequests.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mqtt = require('mqtt');
 var router = express.Router({ mergeParams: true });
 var BookingRequest = require('../models/bookingRequest.js');
 
@@ -62,45 +61,36 @@ router.patch('/:id', function(req, res, next) {
     });
 });
 
-// Post a specific booking/appointement
-router.post("/bookings", function(req, res, next) {
-    var newBooking = new BookingRequest(req.body);
-
-    BookingRequest.user_id = (req.body.user_id || BookingRequest.user_id)
-    BookingRequest.dentist_id = (req.body.dentist_id || BookingRequest.dentist_id)
-    BookingRequest.issuance = (req.body.issuance || BookingRequest.issuance)
-    BookingRequest.date = (req.body.date || BookingRequest.date)
-
-    newBooking.save(function(err, addBooking) {
-        if (err) {
-            return next(err);
-        }
-
-        res.status(201).json(addBooking)
+//Create a booking request
+router.post('/', function(req, res, next){
+    var bookingRequest = new BookingRequest(req.body);
+    bookingRequest.save(function(err) {
+        if (err) { return next(err); }
+        res.status(200).json(bookingRequest);
     })
-})
+});
 
 // Get all appointements
-router.get("/bookingList", async(req, res) => {
+router.get("/", async(req, res) => {
     BookingRequest.find().exec(function(err, results) {
         if (err) { return next(err); }
-        if (!results) { return res.status(404).json({ 'message': 'no events found' }); }
+        if (!results) { return res.status(404).json({ 'message': 'No booking requests found!' }); }
         res.status(200).json(results);
     })
 });
 
 // Delete a specific booking
-router.delete("/bookings/:id", async(req, res) => {
+router.delete("/:id", async(req, res) => {
     const id = req.params.id;
 
-    BookingRequest.findOneAndDelete({ _id: id }, function(err, booking) {
+    BookingRequest.findOneAndDelete({ _id: id }, function(err, bookingRequest) {
         if (err) {
             return next(err);
         }
-
-        if (booking === null) {
-            return res.status(404).json({ "message": "Booking doesn't exist" });
+        if (bookingRequest === null) {
+            return res.status(404).json({ "message": "Booking request doesn't exist" });
         }
+        res.status().json({'message': 'Booking request deleted successfully!'})
     });
 });
 
