5a7c40e181541066cded44b592c24dcf1614e42c
zsolnai <georg.zsolnai123@gmail.com>
2022-11-18 07:07:29 +0100

    #22 Create linkButton component for navbar routing

---
+++ client/src/components/LinkButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@mui/material'
+import { Link, To } from 'react-router-dom'
+
+type Props = {
+  linkTo: To
+  variant: 'text' | 'outlined' | 'contained' | undefined
+  buttonText?: String
+  children?: React.ReactNode
+  icon?: React.ReactNode
+} & React.ComponentProps<typeof Button>
+
+const LinkButton = (props: Props) => {
+  return (
+    <Link to={props.linkTo} style={{ textDecoration: 'none', color: 'black' }}>
+      {props.buttonText ? (
+        <Button {...props}>{props.buttonText}</Button>
+      ) : (
+        <Button variant={props.variant}>{props.children}</Button>
+      )}
+    </Link>
+  )
+}
+
+export default LinkButton
