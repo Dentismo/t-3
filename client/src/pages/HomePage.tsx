a07f7fd488fca9a03740c01aededbdfc5276922f
IVidackovic <gusvidiv@student.gu.se>
2022-11-21 16:27:11 +0100

    #20 Refactor home page interface

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material';
 import DentistCard from '../components/DentistCard';
+import clinic from '../images/elegant-clinic.jpg';
 import '../styles/HomePage.css';
 
 
@@ -9,9 +10,9 @@ function HomePage() {
         {name: "Dentist 1", details: "08.00 to 17:00 Monday to Friday", id: "1", location: "Lindholmen"}, 
         {name: "Dentist 2"}, 
         {name: "Dentist 3"}, 
-        {name: "Dentist 4"}]
-    
-    
+        {name: "Dentist 4"},
+        {name: "Dentist 5"}]
+
     return (
     <div>
         <div className='main-container'>
@@ -19,21 +20,27 @@ function HomePage() {
                 <h1 className="dentist-heading"> Dentismo </h1>
             </div>
             <div className='home-image-holder'>
-                <p className='image-text'>Image here</p>
-            </div>
-            <div className="welcome-button">
-                <div>
-                    <h3>Welcome to Dentismo</h3>
+                <div className='text-button-holder'>
+                    <div className='text-holder'>
+                        <span>Every dental experience personalized to you.</span>
+                    </div>
+                    <div className='button-holder'>
+                        <Button className="dentist-button" variant="outlined"> See available dentist</Button>
+                    </div>
                 </div>
-                <div>
-                    <Button variant="outlined">{buttonText}</Button>
-                </div> 
+                <div className='image-holder'>
+                    <img src={clinic} alt="" />
+                </div>
             </div>
-            <div style={{border: "1px solid black", marginTop: "2rem", marginBottom: "2rem"}}></div>
+            <div className="banner-below-image">
+                <div>
+                    <h2 className='banner-text'>Dentismo is a web-service based in Gothenburg, to provide available dentist to customers.</h2>
+                </div>
+            </div>
             <div className="available-dentist">
                 <p>Available Dentists:</p>
             </div>
-            <div className={dentists.length > 3 && dentists.length !== 5 ? 'dentist-cards' : "alt-dentist-cards"}>
+            <div id='card-section' className={dentists.length > 3 && dentists.length !== 5 ? 'dentist-cards' : "alt-dentist-cards"}>
             {dentists.map(dentist => <DentistCard dentist={dentist} key={dentist.name}></DentistCard>)}
             </div>
             
