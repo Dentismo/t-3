8c1506fa4b395c54b81e3fde79f0ea1d020d40b5
dovhun <dovhun@student.chalmers.se>
2022-11-23 14:08:24 +0100

    #19 Added an image of a mouth with missing teeth and made the body smack centre of the main party of the site

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,20 +1,39 @@
 import Navbar from '../components/Navbar'
-import { Button } from '@mui/material'
+import { styled } from '@mui/material'
 import DentistCard from '../components/DentistCard'
 import '../styles/HomePage.css'
+import logo from '../images/parodontitis.png'
+
 import { NoEncryption } from '@mui/icons-material'
 
 function ErrorPage() {
+  const ErrorImage = styled('div')({
+    margin: "auto",
+  })
+
+  const Body = styled("div")({
+    margin: "10%",
+  })
+
   return (
     <div>
-      <div className="error-page" style = {{
-        textAlign: 'center'
-      }}>
-        <h1>Error 404: Page not found</h1>
-        <a href="/" style={{
-          color: 'blue'
-        }}>Back home</a>
-      </div>
+      <Body>
+        <div className="error-page" style = {{
+          textAlign: 'center'
+        }}>
+          <h1>Error 404: Page not found</h1>
+          <a href="/" style={{
+            color: 'blue'
+          }}>Back home</a>
+          <ErrorImage>
+          <img 
+            src={logo} 
+            alt="image" 
+            style={{width:"300px", height:"300px"}}
+            />
+          </ErrorImage>
+        </div>
+      </Body>
     </div>
   )
 }
