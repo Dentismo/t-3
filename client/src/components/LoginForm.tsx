68c09fe28d2f059675ae2b275ac655ca9ea33fe1
IVidackovic <gusvidiv@student.gu.se>
2022-11-24 19:42:23 +0100

    #24 Improve validation checker with formik

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,11 +1,21 @@
 import { ThemeProvider } from "@emotion/react";
 import { Button, createTheme, Grid, Paper, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+
 
 const paperStyle = {padding: 20, height: '18rem', width: '25rem'}
 const textStyle = {margin: 4}
 
-const theme = createTheme({
+const styles = {
+    form: {
+        display: 'flex',
+        flexDirection: 'column' as 'column'
+    }
+}
+
+const theme = createTheme(  {
     typography: {
         fontFamily: [
             '-Playfair-Display'
@@ -13,33 +23,65 @@ const theme = createTheme({
     },
 });
 
-function LoginForm() {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    const[error, setError] = useState(false);
-    const[passwordError, setPasswordError] = useState(false);
+    function LoginForm() {
+        const navigate = useNavigate();
 
-    const loginClick = () => {
-        if(!email) {
-            setError(true);
-            return null;
-        } 
-            setError(false);
-            return null;
-        
-    };
+        const formik = useFormik({
+            initialValues: {
+                email: '',
+                password: ''
+            },
+            validationSchema: Yup.object({
+                email: Yup.string()
+                .email("Invalid Email")
+                .required("Required"),
+                
+                password: Yup.string()
+                .required("Required")
+            }),
+            onSubmit: (values, {resetForm}) => {
+                if(values.email === 'ivan@gmail.com' && values.password === 'ivan123') {
+                    ///navigate("/dashboard")
+                    resetForm()
+                }
+            }
+        });
 
     return (
         <ThemeProvider theme={theme}>
         <Grid textAlign='center'>
             <Paper elevation={10} style={paperStyle}>
-                <Grid textAlign='center'>
-                    
+                <Grid textAlign='center'>  
                     <Typography variant='h3'>Login</Typography>
                 </Grid>
-                <TextField style={textStyle} label='Email' placeholder="Enter Email" fullWidth required value={email} error={error} name='email' onChange={(e) => setEmail(e.target.value)}></TextField>
-                <TextField style={textStyle} label='Password' placeholder="Enter Password" type='password' fullWidth required value={password} error={error} name='password' onChange={(e) => setPassword(e.target.value) }></TextField>
-                <Button onClick={loginClick} type='submit' color='primary' variant='contained' style={{margin: '8px 0', backgroundColor: '#22443d'}} >Login</Button>
+                <form onSubmit={formik.handleSubmit}>
+                    <div style={styles.form}>
+                        <TextField 
+                            id='email' 
+                            name='email' 
+                            type='email' 
+                            variant='outlined'
+                            error={formik.touched.email} 
+                            placeholder="Enter Email"
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange} 
+                            value={formik.values.email}>
+                        </TextField>
+                        {formik.touched.email && formik.errors.email ? <span style={{color: 'red', fontFamily: '-Playfair-Display', textAlign: "left"}}>{formik.errors.email}</span> : null}
+                        <TextField 
+                            id='password' 
+                            name='password' 
+                            type='password'
+                            error={formik.touched.password} 
+                            placeholder="Enter Password" 
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange} 
+                            value={formik.values.password}>
+                        </TextField>
+                        {formik.touched.password && formik.errors.password ? <span style={{color: 'red', fontFamily: '-Playfair-Display', textAlign: "left"}}>{formik.errors.password}</span> : null}
+                    </div>
+                    <Button type='submit' color='primary' variant='contained' style={{margin: '8px 0', backgroundColor: '#22443d'}} >Login</Button>
+                </form>
             </Paper>
         </Grid>
         </ThemeProvider>
