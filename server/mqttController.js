fc58cbc5b96abadb3755afb6421afe7966109a15
bardiaf <bardiaf@student.chalmers.se>
2022-12-13 11:47:17 +0100

    #37 Add circuit breaker for http GET route

--- server/mqttController.js
+++ server/mqttController.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const mqttHandler = require("./mqttHandler");
 const bcrypt = require('bcryptjs');
+const CircuitBreaker = require('opossum');
+
 
 const configurations = {
   timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
@@ -29,7 +31,36 @@ router.post("/request/:topic/:id", async (req, res) => {
   res.status(201).json(response);
 });
 
-router.get("/request/:topic/:topicDefinition?/:id", async (req, res) => {
+// router.get("/request/:topic/:topicDefinition?/:id", async (req, res) => {
+//   //define mqtt topics with the given parametere
+//   if(req.params.topicDefinition) {
+//     const mqttTopic = "request/" + req.params.topic + "/" + req.params.topicDefinition + "/" + req.params.id
+//     const responseTopic = "response/" + req.params.topic + "/" + req.params.topicDefinition + "/" + req.params.id
+
+//     mqttHandler.publish(mqttTopic, req.params.id);
+
+//   //subscribe to the response
+//   mqttHandler.subscribe(responseTopic);
+//   //message received is parse to json and returned to the frontend
+//   const response = await mqttHandler.onMessage();
+
+//     res.status(201).json(response);
+//   } else {
+//     const mqttTopic = "request/" + req.params.topic + "/" + req.params.id
+//     const responseTopic = "response/" + req.params.topic + "/" + req.params.id
+
+//     mqttHandler.publish(mqttTopic, req.params.id);
+
+//     //subscribe to the response
+//     mqttHandler.subscribe(responseTopic);
+
+//     //message received is parse to json and returned to the frontend
+//     const response = await mqttHandler.onMessage();
+
+//     res.status(201).json(response);
+// }
+// });
+const breaker2 = new CircuitBreaker(router.get("/request/:topic/:topicDefinition?/:id", async (req, res) => {
   //define mqtt topics with the given parametere
   if(req.params.topicDefinition) {
     const mqttTopic = "request/" + req.params.topic + "/" + req.params.topicDefinition + "/" + req.params.id
@@ -37,10 +68,10 @@ router.get("/request/:topic/:topicDefinition?/:id", async (req, res) => {
 
     mqttHandler.publish(mqttTopic, req.params.id);
 
-  //subscribe to the response
-  mqttHandler.subscribe(responseTopic);
-  //message received is parse to json and returned to the frontend
-  const response = await mqttHandler.onMessage();
+    //subscribe to the response
+    mqttHandler.subscribe(responseTopic);
+    //message received is parse to json and returned to the frontend
+    const response = await mqttHandler.onMessage();
 
     res.status(201).json(response);
   } else {
@@ -55,9 +86,9 @@ router.get("/request/:topic/:topicDefinition?/:id", async (req, res) => {
     //message received is parse to json and returned to the frontend
     const response = await mqttHandler.onMessage();
 
-    res.status(201).json(response);
-}
-});
+    //res.status(201).json(response);
+  }
+}), configurations).fire( "response/" + req.params.topic + "/" + req.params.topicDefinition + "/" + req.params.id).then(res.status(201).json(response)).catch(console.error);
 
 router.patch("/request/:delegation/:id", async (req, res) => {
   const { delegation } = req.params;
